bf5a7b518ff94f1667ac36a5b1ed2ea6eeadfa92
BackCoder <skystom2@naver.com>
2022-08-09 22:20:41 +0900

    delete useless dbconnector

--- src/main/java/Servlet/DAO/memberDAO.java
+++ src/main/java/Servlet/DAO/memberDAO.java
@@ -45,52 +45,6 @@ public class memberDAO {
 
         return list;
     }
-/*
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.err.println("JDBC 드라이버를 로드하는데에 문제 발생" + e.getMessage());
-            e.printStackTrace(); }
-
-
-
-        try {
-            con = DriverManager.getConnection
-                    ("jdbc:mysql://127.0.0.1:3306/backcoder", "backcoder", "1111");
-            System.out.println("연결 완료!");
-
-//sql
-
-            String sql = "select * from servletmember";
-
-            PreparedStatement pr = con.prepareStatement(sql);
-
-            ResultSet resultSet = pr.executeQuery();
-
-
-            while (resultSet.next()) {
-
-                memberDTO dto = new memberDTO(resultSet.getString(1), resultSet.getString(2),
-                        resultSet.getString(3));
-
-                list.add(dto);
-            }
-
-        } catch (SQLException e) {
-            System.err.println("연결 오류" + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            try {
-                con.close();
-                System.out.println("연결 해제 성공!");
-            } catch (SQLException e) {
-            }
-        }
-        return list;
-    }
-
-*/
-
 
 
     public void join(memberDTO dto) {
@@ -125,46 +79,6 @@ public class memberDAO {
 
 
     }
-     /*   try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.err.println("JDBC 드라이버를 로드하는데에 문제 발생" + e.getMessage());
-            e.printStackTrace(); }
-
-
-        try {
-            con = DriverManager.getConnection
-                    ("jdbc:mysql://127.0.0.1:3306/backcoder", "backcoder", "1111");
-            System.out.println("연결 완료!");
-
-//sql
-
-            String sql = "insert into servletmember values(?,?,?)";
-
-            PreparedStatement pr = con.prepareStatement(sql);
-
-            pr.setString(1,dto.getId());
-            pr.setString(2,dto.getPw());
-            pr.setString(3,dto.getName());
-
-            int joinResult = pr.executeUpdate();
-
-            condition = joinResult;
-
-
-        } catch (SQLException e) {
-            System.err.println("연결 오류" + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            try {
-                con.close();
-                System.out.println("연결 해제 성공!");
-            } catch (SQLException e) {
-            }
-        }
-
-    }
-*/
 
     public int delete(String userid) {
 
@@ -195,44 +109,6 @@ public class memberDAO {
 
         return deleteResult;
     }
-        /*try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.err.println("JDBC 드라이버를 로드하는데에 문제 발생" + e.getMessage());
-            e.printStackTrace(); }
-
-
-        try {
-            con = DriverManager.getConnection
-                    ("jdbc:mysql://127.0.0.1:3306/backcoder", "backcoder", "1111");
-            System.out.println("연결 완료!");
-
-//sql
-
-            String sql = "delete from servletmember where id = ?";
-
-            PreparedStatement pr = con.prepareStatement(sql);
-
-            pr.setString(1, userid);
-
-            deleteResult = pr.executeUpdate();
-
-
-        } catch (SQLException e) {
-            System.err.println("연결 오류" + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            try {
-                con.close();
-                System.out.println("연결 해제 성공!");
-            } catch (SQLException e) {
-            }
-        }
-
-        return deleteResult;
-    }
-
-*/
 
 
     public int login(String userid, String userpw) {
@@ -273,59 +149,6 @@ finally { try {con.close(); } catch (Exception e) {} }
         return condition;
     }
 
-        /*
-        Connection con = null;
-        int condition = 0;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.err.println("JDBC 드라이버를 로드하는데에 문제 발생" + e.getMessage());
-            e.printStackTrace(); }
-
-
-        try {
-            con = DriverManager.getConnection
-                    ("jdbc:mysql://127.0.0.1:3306/backcoder", "backcoder", "1111");
-            System.out.println("연결 완료!");
-
-//sql
-
-            String sql = "select id, pw from servletmember where id=?";
-
-            PreparedStatement pt = con.prepareStatement(sql);
-            pt.setString(1,userid);
-            ResultSet rs = pt.executeQuery();
-
-            String dbid= null;
-            String dbpw = null;
-            if (rs.next()) {
-                condition=1;
-
-                dbid = rs.getString("id");
-                dbpw = rs.getString("pw");
-
-                if (dbpw.equals(userpw)) {
-                    condition=2;
-                }
-            } else {condition = 3; }
-
-        } catch (SQLException e) {
-            System.err.println("연결 오류" + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            try {
-                con.close();
-                System.out.println("연결 해제 성공!");
-            } catch (SQLException e) {
-            }
-        }
-        return condition;
-    }
-*/
-
-
-
 
     public int edit(memberDTO dto) {
 
@@ -357,10 +180,6 @@ finally { try {con.close(); } catch (Exception e) {} }
         return editResult;
 
     }
-
-
-
-
 
 
 //
